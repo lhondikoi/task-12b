66acd124d54b0d5dfd5e9b16ad5459e8cfeaeb9e
lhondikoi <l.hondikoi@gmail.com>
2023-12-13 15:17:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let query = document.querySelector('#query')
+let resultContainer = document.querySelector('.search-results')
+
+function search() {
+    // delete previous search results
+    resultContainer.innerHTML = ""
+    
+    let searchQuery = query.value
+    query.value = ""
+    if (searchQuery !== "") {
+        fetch('http://universities.hipolabs.com/search?' + new URLSearchParams({
+            name: searchQuery
+        }))
+        .then(r => r.json())
+        .then(d => {
+            for (let resultItem of d) {
+                let div = document.createElement('div')
+                div.classList.add('result-item')
+
+                let title = document.createElement('h3')
+                title.classList.add('title')
+
+                let link = document.createElement('a')
+                link.setAttribute('href', resultItem.web_pages[0])
+                link.setAttribute('target', '_blank')
+
+                link.innerText = resultItem.name
+
+                title.appendChild(link)
+
+                let location = document.createElement('span')
+                location.innerHTML = `<i class="bi bi-geo-alt"></i> ${resultItem.country}`
+
+                div.appendChild(title)
+                div.appendChild(location)
+                resultContainer.appendChild(div)
+            }
+        })
+    }
+}
+
+query.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        search()
+    }
+})
